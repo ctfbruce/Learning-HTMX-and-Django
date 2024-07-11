1a48499609381b7afef6e57a3c7fadf03935db69
TheodorMattliGITHUB <140241289+TheodorMattliGITHUB@users.noreply.github.com>
2024-07-11 18:49:16 +0200

    segmented header and hero parts from the base html into a different folder, also made home.html which extends base

--- a_posts/views.py
+++ a_posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home_view(request):
+    
+    context = {"home_page_title":"DJ Unchained"}
+    return render(request,"a_posts/home.html", context=context)
